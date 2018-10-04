98ce69bab43d0665d51ae1bd771bf5f51f177061
Nicolò Vescera <nicolo.vescera@gmail.com>
2018-10-04 17:26:43 +0200

    Sistemato codice

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
+'''
+    Silence is -36, no sound -80, max sound 0
+    -20 is more less a talking person [shoud test this]
+    should check rate on stream
+'''
 import numpy
 import pyaudio
-
-#Silence is -36, no sound -80, max sound 0
-#-20 is more less a talking person [shoud test this]
 import analyse
-
 import time
 
 # Initialize PyAudio
 pyaud = pyaudio.PyAudio()
 
 # Open input stream, 16-bit mono at 44100 Hz
-# On my system, device 2 is a USB microphone, your number may differ.
 stream = pyaud.open(
     format = pyaudio.paInt16,
     channels = 1,
@@ -20,9 +20,9 @@ stream = pyaud.open(
     input_device_index = 0,
     input = True)
 
-runner = True   #contro variable for Loop
-count = 0       #count number of bits
-bits = ""       #final 8bit string
+runner = True   #Control variable for Loop
+count = 0       #Count number of bits
+bits = ""       #Final 8bit string
 while runner:
     try:
         # Read raw microphone data
@@ -31,7 +31,7 @@ while runner:
         # Convert raw data to NumPy array
         samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
 
-        # Show the volume and pitch
+        #Convert NumPy array to db value (float and less than 0)
         sound = analyse.loudness(samps)
 
         #If talking record 1
@@ -40,27 +40,25 @@ while runner:
         else:
             bits += "0"
 
-        #format output
-        if count == 7: #reached 8 bits
+        #Format output
+        if count == 7: #Reached 8 bits
             print bits
             bits = ""
             count = 0
         else:
             count += 1
 
-        #set some delay, maybe it is 1 sec
+        #Set some delay, maybe it is 1 sec
         stream.stop_stream();
-
         #time.sleep(.5)
-
         stream.start_stream()
 
     except KeyboardInterrupt:
-        runner = False  #stop script while pressing ctrl + c
+        runner = False  #Stop script while pressing ctrl + c
 
-# stop stream
+#Stop stream
 stream.stop_stream()
 stream.close()
 
-# close PyAudio
+#Close PyAudio
 pyaud.terminate()
