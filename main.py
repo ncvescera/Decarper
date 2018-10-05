0bcf4b2b0a5d54b71d57b785ce508329b3cf5882
Nicolò Vescera <nicolo.vescera@gmail.com>
2018-10-05 19:18:00 +0200

    Modificata logica del programma

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/
 '''
     Silence is -36, no sound -80, max sound 0
     -20 is more less a talking person [shoud test this]
-    should check rate on stream
 '''
 import numpy
 import pyaudio
 import analyse
+import sys
 import time
+import datetime
+
+from decoding import *
 
 # Initialize PyAudio
 pyaud = pyaudio.PyAudio()
 
 # Open input stream, 16-bit mono at 44100 Hz
+# On my system, device 2 is a USB microphone, your number may differ.
 stream = pyaud.open(
     format = pyaudio.paInt16,
     channels = 1,
@@ -20,45 +24,67 @@ stream = pyaud.open(
     input_device_index = 0,
     input = True)
 
-runner = True   #Control variable for Loop
-count = 0       #Count number of bits
-bits = ""       #Final 8bit string
+runner = True   #contro variable for Loop
+count = 0       #count number of bits
+bits = ""       #final 8bit string
+phrase = ""     #la frase di carpi che viene aggiornata runtime
+tres = -20      #treshold
+
 while runner:
     try:
-        # Read raw microphone data
-        rawsamps = stream.read(1024)
+            while count < 7:
+                time.sleep(0.1)
 
-        # Convert raw data to NumPy array
-        samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+                stream.start_stream()
+                rawsamps = stream.read(1024)
+                samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+                sound = analyse.loudness(samps)
+                stream.stop_stream()
 
-        #Convert NumPy array to db value (float and less than 0)
-        sound = analyse.loudness(samps)
+                if sound > tres:
+                    bits += "1"
+                else:
+                    bits += "0"
 
-        #If talking record 1
-        if sound > -20:
-            bits += "1"
-        else:
-            bits += "0"
+                count += 1
+                if(count == 7):
+                    if bits == "0000000":
+                        phrase += " "
+                    else:
+                        if bits == "1111111":
+                            phrase += "!"
+                        else:
 
-        #Format output
-        if count == 7: #Reached 8 bits
-            print bits
-            bits = ""
+                            phrase += text_from_bits(bits)
+
+                    print bits
+                    print "Carpi said: " + phrase
+                    
+            #sys.stdout.write(bits+"\n")
+            #sys.stdout.write("Carpi said: " + phrase+"\n")
+            #sys.stdout.write("\r")
+            #sys.stdout.flush()
+            #print "\r"+phrase
+
             count = 0
-        else:
-            count += 1
-
-        #Set some delay, maybe it is 1 sec
-        stream.stop_stream();
-        #time.sleep(.5)
-        stream.start_stream()
+            bits = ""
 
     except KeyboardInterrupt:
-        runner = False  #Stop script while pressing ctrl + c
+        runner = False  #stop script while pressing ctrl + c
 
-#Stop stream
+#Writing log
+file = open("log","a")
+
+data = datetime.datetime.now()
+file.write("Message from Carpi to Earth - " + str(data) + "\n")
+file.write(phrase)
+file.write("\n\n")
+
+file.close()
+
+# stop stream
 stream.stop_stream()
 stream.close()
 
-#Close PyAudio
+# close PyAudio
 pyaud.terminate()
